915038fad684963162b000a63d49714e0e0c612a
Thiru <tkanesh@ntu.edu.sg>
2019-06-13 14:52:43 +0800

    removed build/ directory

--- build/classes/ntu/celt/eUreka2/components/SearchForm.js
+++
@@ -1,8 +0,0 @@
-function inputTxtFocus(elem , defaultTxt){
-	if(elem.value==defaultTxt)
-		elem.value = "";
-}
-function inputTxtBlur(elem , defaultTxt){
-	if(elem.value=="")
-		elem.value = defaultTxt;
-}
